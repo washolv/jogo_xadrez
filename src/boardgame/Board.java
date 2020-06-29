2e1354675e4cfbcf86604b7a8e94273dbd5d1667
washolv <w_olv@hotmail.com>
2020-06-29 18:49:44 -0300

    inicial board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -10,6 +10,7 @@ package boardgame;
  * @author w_olv
  */
 public class Board {
+
     private int rows;
     private int columns;
     private Piece[][] pieces;
@@ -35,7 +36,12 @@ public class Board {
     public void setColumns(int columns) {
         this.columns = columns;
     }
-    
-    
-    
+
+    public Piece piece(int row, int column) {
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
 }
